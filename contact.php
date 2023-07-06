6e91df0f04550f0ae53d7b240b41c197852975fd
hhh99-hub <hemang.huria13@gmail.com>
2023-07-06 11:39:22 +0530

    replaced images

--- contact.php
+++ contact.php
@@ -1,26 +1,25 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Get form data
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $subject = $_POST["subject"];
-  $message = $_POST["message"];
+  // Retrieve form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-  // Set recipient email address
-  $to = "hemang.huria13@gmail.com"; // Replace with your own email address
+  // Perform any necessary form validation and data processing here
+  // ...
 
-  // Set email headers
-  $headers = "From: $name <$email>" . "\r\n";
-  $headers .= "Reply-To: $email" . "\r\n";
-  $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+  // Send an email with the form data
+  $to = 'hemang.huria13@gmail.com'; // Replace with your email address
+  $subject = 'New form submission';
+  $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message";
 
-  // Send email
-  if (mail($to, $subject, $message, $headers)) {
-    echo "success";
+  if (mail($to, $subject, $body)) {
+    // Email sent successfully
+    echo "Form submitted successfully. Thank you!";
   } else {
-    echo "Error sending email.";
+    // Error sending email
+    echo "An error occurred while submitting the form. Please try again.";
   }
-} else {
-  echo "Invalid request.";
 }
 ?>
